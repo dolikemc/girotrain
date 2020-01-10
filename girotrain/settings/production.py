02ee6e480f1890d7fbda67496f3c6ef1e5055bdf
chris_mit <chr@buchetmann.de>
2020-01-10 09:26:58 +0100

    (mysql.W002) MySQL Strict Mode is not set for database connection 'default'

--- girotrain/settings/production.py
+++ girotrain/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4@h0kyn$tum8_&rlrg*lyn!i@yy)f@utzwqly)ja+($kdsuz+u'
 
-#SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['www.buchetmann.org']
@@ -16,6 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': '/var/www/ssd1470/priv/db.conf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
@@ -25,7 +26,6 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/var/www/ssd1470/htdocs/wagtail/media/'
 MEDIA_URL = '/media/'
-
 
 try:
     from .local import *
