ffe9542898a1032c5b3570b353434af5eac3d75a
chris_mit <chr@buchetmann.de>
2020-02-17 15:46:31 +0100

    add stream page, remove links from home page

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from modelcluster.models import ClusterableModel, ParentalKey
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, RichTextField, FieldRowPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, RichTextField, FieldRowPanel, StreamFieldPanel
 from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.models import register_snippet
@@ -16,17 +19,11 @@ class HomePage(Page):
                                 features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'hr', 'link'])
     body_top_it = RichTextField(blank=True, default="",
                                 features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'hr', 'link'])
-    # optional link after the 1st body
-    link_top = models.URLField(blank=True)
-    link_text_top = models.CharField(max_length=256, blank=True)
     # 2nd body bilingual
     body_bottom_de = RichTextField(blank=True, default="",
                                    features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'hr', 'link'])
     body_bottom_it = RichTextField(blank=True, default="",
                                    features=['h2', 'h3', 'bold', 'italic', 'ol', 'ul', 'hr', 'link'])
-    # optional link after the 2nd body
-    link_bottom = models.URLField(blank=True)
-    link_text_bottom = models.CharField(max_length=256, blank=True)
     content_panels = [
         FieldRowPanel(
             [
@@ -40,9 +37,6 @@ class HomePage(Page):
                 FieldPanel('body_top_it'),
             ]
         ),
-        FieldRowPanel([
-            FieldPanel('link_top'), FieldPanel('link_text_top')
-        ]),
         InlinePanel('home_page_images', label="Images"),
         FieldRowPanel(
             [
@@ -50,10 +44,6 @@ class HomePage(Page):
                 FieldPanel('body_bottom_it'),
             ]
         ),
-
-        FieldRowPanel([
-            FieldPanel('link_bottom'), FieldPanel('link_text_bottom')
-        ]),
 
     ]
     translated_title = TranslatedField(
@@ -182,4 +172,33 @@ class AccordionEntry(Orderable):
     show_in_menus_default = True
 
     def __str__(self) -> str:
-        return self.body_de
+        return self.header
+
+
+class StreamPage(Page):
+    # title is used for title in German
+    title_it = models.CharField(max_length=256, blank=True)
+    # 1st body bilingual
+    body = StreamField([
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
+    content_panels = [
+        FieldRowPanel(
+            [
+                FieldPanel('title'),
+                FieldPanel('title_it'),
+            ]
+        ),
+        StreamFieldPanel('body')
+    ]
+    translated_title = TranslatedField(
+        'title',
+        'title_it',
+    )
+
+    show_in_menus_default = True
+
+    def __str__(self) -> str:
+        return self.title
